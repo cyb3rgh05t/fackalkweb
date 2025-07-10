779dc0d919d664d2d300db528e60ff7b597a31c2
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2025-07-10 14:50:06 +0200

    reverse

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-import { showSection, loadGlobalSettings } from "./utils.js";
+import { showSection } from "./utils.js";
 import { loadDashboard } from "./dashboard.js";
 import { loadKunden } from "./kunden.js";
 import { loadFahrzeuge } from "./fahrzeuge.js";
@@ -6,24 +6,14 @@ import { loadAuftraege } from "./auftraege.js";
 import { loadRechnungen } from "./rechnungen.js";
 import { loadEinstellungen } from "./einstellungen.js";
 
-// Globale Funktionen für HTML onclick events verfügbar machen
-window.showSection = showSection;
+window.showSection = showSection; // Damit Buttons im HTML es aufrufen können
 
-document.addEventListener("DOMContentLoaded", async () => {
-  console.log("App wird initialisiert...");
-
-  // Zuerst Einstellungen laden
-  await loadGlobalSettings();
-
-  // Dann alle Module initialisieren
-  await Promise.all([
-    loadDashboard(),
-    loadKunden(),
-    loadFahrzeuge(),
-    loadAuftraege(),
-    loadRechnungen(),
-    loadEinstellungen(),
-  ]);
-
-  console.log("App erfolgreich initialisiert");
+document.addEventListener("DOMContentLoaded", () => {
+  loadDashboard();
+  loadKunden();
+  loadFahrzeuge();
+  loadAuftraege();
+  loadRechnungen();
+  loadEinstellungen();
+  // Sections erst bei Bedarf nachladen
 });
